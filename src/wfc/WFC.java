2a1d2db2b3f09c68836c8a82aab2684417a7194e
Hamiduzzaman Noyon <hn22aah@herts.ac.uk>
2023-04-08 12:35:27 +0100

    Generate reports updated, added month section on bookings, user input section updated, add a customer function added

--- src/wfc/WFC.java
+++ src/wfc/WFC.java
@@ -23,12 +23,13 @@ public class WFC {
         int choice;
         do {
             System.out.println("\n");
-            System.out.println("1. View timetable by day");
-            System.out.println("2. View timetable by fitness type");
-            System.out.println("3. Book a lesson");
-            System.out.println("4. Cancel a booking");
-            System.out.println("5. Write a review");
-            System.out.println("6. Generate reports");
+            System.out.println("1. Add a Customer");
+            System.out.println("2. View timetable by day");
+            System.out.println("3. View timetable by fitness type");
+            System.out.println("4. Book a lesson");
+            System.out.println("5. Cancel a booking");
+            System.out.println("6. Write a review");
+            System.out.println("7. Generate reports");
             System.out.println("0. Exit");
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
@@ -36,69 +37,50 @@ public class WFC {
 
             switch (choice) {
                 case 1:
-                    System.out.print("Select a day: ");
-                    System.out.println("1. Saturday");
-                    System.out.println("2. Sunday");
-                    System.out.print("Enter the number: ");
-                    int dayIndex = scanner.nextInt();
-                    scanner.nextLine();  // Consume the newline character
-                    String[] days = {"Saturday", "Sunday",};
-                    String day = days[dayIndex - 1];
-                    fitnessClub.displayTimetable(day);
+                    System.out.print("Enter customer name: ");
+                    String cName = scanner.nextLine();
+                    System.out.print("Enter customer email: ");
+                    String cEmail = scanner.nextLine();
+                    fitnessClub.addCustomer(new Customer(cName, cEmail));
                     break;
                 case 2:
-                    System.out.println("Select a fitness type:");
-                    System.out.println("1. SPIN");
-                    System.out.println("2. YOGA");
-                    System.out.println("3. BODYSCULPT");
-                    System.out.println("4. ZUMBA");
-                    System.out.println("5. CYCLE RACE");
-                    System.out.print("Enter the number: ");
-                    int fitnessTypeIndex = scanner.nextInt();
-                    scanner.nextLine();  // Consume the newline character
-                    String[] fitnessTypes = {"SPIN", "YOGA", "BODYSCULPT", "ZUMBA", "CYCLE RACE"};
-                    String fitnessType = fitnessTypes[fitnessTypeIndex - 1];
-                    fitnessClub.displayTimetable(fitnessType);
+                    String day = selectDay(scanner);
+                    fitnessClub.displayTimetable(day);
                     break;
                 case 3:
+                    String fitnessType = selectFitnessType(scanner);
+                    fitnessClub.displayTimetable(fitnessType);
+                    break;
+                case 4:
                     System.out.print("Enter customer name: ");
                     String customerName = scanner.nextLine();
                     System.out.print("Enter customer email: ");
                     String customerEmail = scanner.nextLine();
-                    System.out.print("Enter day (Saturday or Sunday): ");
-                    day = scanner.nextLine();
-                    System.out.print("Enter time (morning or afternoon): ");
-                    String time = scanner.nextLine();
-                    System.out.print("Enter fitness type: ");
-                    fitnessType = scanner.nextLine();
-                    fitnessClub.bookLesson(customerEmail, day, time, fitnessType);
-                    break;
-                case 4:
-                    System.out.print("Enter customer email: ");
-                    customerEmail = scanner.nextLine();
-                    System.out.print("Enter day (Saturday or Sunday): ");
-                    day = scanner.nextLine();
-                    System.out.print("Enter time (morning or afternoon): ");
-                    time = scanner.nextLine();
-                    System.out.print("Enter fitness type: ");
-                    fitnessType = scanner.nextLine();
-                    fitnessClub.cancelBooking(customerEmail, day, time, fitnessType);
+                    String dayBook = selectDay(scanner);
+                    String time = selectTime(scanner);
+                    String fitnessTypeBook = selectFitnessType(scanner);
+                    fitnessClub.bookLesson(customerEmail, dayBook, time, fitnessTypeBook);
                     break;
                 case 5:
                     System.out.print("Enter customer email: ");
                     customerEmail = scanner.nextLine();
-                    System.out.print("Enter day (Saturday or Sunday): ");
-                    day = scanner.nextLine();
-                    System.out.print("Enter time (morning or afternoon): ");
-                    time = scanner.nextLine();
-                    System.out.print("Enter fitness type: ");
-                    fitnessType = scanner.nextLine();
+                    String dayCancel = selectDay(scanner);
+                    String timeCancel = selectTime(scanner);
+                    String fitnessTypeCancel = selectFitnessType(scanner);
+                    fitnessClub.cancelBooking(customerEmail, dayCancel, timeCancel, fitnessTypeCancel);
+                    break;
+                case 6:
+                    System.out.print("Enter customer email: ");
+                    customerEmail = scanner.nextLine();
+                    String dayReveiw = selectDay(scanner);
+                    String timeReview = selectTime(scanner);
+                    String fitnessTypeReview = selectFitnessType(scanner);
                     System.out.print("Enter rating (1-5): ");
                     int rating = scanner.nextInt();
                     scanner.nextLine();  // Consume the newline character
-                    fitnessClub.writeReview(customerEmail, day, time, fitnessType, rating);
+                    fitnessClub.writeReview(customerEmail, dayReveiw, timeReview, fitnessTypeReview, rating);
                     break;
-                case 6:
+                case 7:
                     fitnessClub.generateReports();
                     break;
                 case 0:
@@ -110,5 +92,68 @@ public class WFC {
         } while (choice != 0);
 
         scanner.close();
+    }
+
+    public static String selectDay(Scanner scanner) {
+        String[] days = {"Saturday", "Sunday"};
+        int dayIndex;
+
+        do {
+            System.out.println("Select a day: ");
+            System.out.println("1. Saturday");
+            System.out.println("2. Sunday");
+            System.out.print("Enter the number: ");
+            dayIndex = scanner.nextInt();
+
+            if (dayIndex < 1 || dayIndex > days.length) {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        } while (dayIndex < 1 || dayIndex > days.length);
+
+        scanner.nextLine(); // Consume the newline character
+        return days[dayIndex - 1];
+    }
+
+    public static String selectTime(Scanner scanner) {
+        String[] times = {"10.00.00", "12.00.00"};
+        int timeIndex;
+
+        do {
+            System.out.println("Select a time: ");
+            System.out.println("1. 10.00");
+            System.out.println("2. 12.00");
+            System.out.print("Enter the number: ");
+            timeIndex = scanner.nextInt();
+
+            if (timeIndex < 1 || timeIndex > times.length) {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        } while (timeIndex < 1 || timeIndex > times.length);
+
+        scanner.nextLine(); // Consume the newline character
+        return times[timeIndex - 1];
+    }
+
+    public static String selectFitnessType(Scanner scanner) {
+        String[] fitnessTypes = {"SPIN", "YOGA", "BODYSCULPT", "ZUMBA", "CYCLE RACE"};
+        int fitnessTypeIndex;
+
+        do {
+            System.out.println("Select a fitness type:");
+            System.out.println("1. SPIN");
+            System.out.println("2. YOGA");
+            System.out.println("3. BODYSCULPT");
+            System.out.println("4. ZUMBA");
+            System.out.println("5. CYCLE RACE");
+            System.out.print("Enter the number: ");
+            fitnessTypeIndex = scanner.nextInt();
+
+            if (fitnessTypeIndex < 1 || fitnessTypeIndex > fitnessTypes.length) {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        } while (fitnessTypeIndex < 1 || fitnessTypeIndex > fitnessTypes.length);
+
+        scanner.nextLine(); // Consume the newline character
+        return fitnessTypes[fitnessTypeIndex - 1];
     }
 }
